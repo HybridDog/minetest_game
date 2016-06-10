6f71ebaad6297f510046dc1c984eeb20811892c6
HybridDog <ovvv@web.de>
2016-06-10 12:11:02 +0200

    bouncy beds
    M  mods/beds/api.lua

--- mods/beds/api.lua
+++ mods/beds/api.lua
@@ -37,8 +37,8 @@ function beds.register_bed(name, def)
 		is_ground_content = false,
 		stack_max = 1,
 		groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 3,
-			bed = 1, fall_damage_add_percent = -92},
-		sounds =  def.sounds or default.node_sound_wood_defaults(),
+			bed = 1, fall_damage_add_percent = -92, bouncy = 62},
+		sounds = def.sounds or default.node_sound_wood_defaults(),
 		node_box = {
 			type = "fixed",
 			fixed = def.nodebox.bottom,
@@ -158,7 +158,8 @@ function beds.register_bed(name, def)
 		is_ground_content = false,
 		pointable = false,
 		groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 3, bed = 2,
-				not_in_creative_inventory = 1, fall_damage_add_percent = -92},
+				not_in_creative_inventory = 1,
+				fall_damage_add_percent = -92, bouncy = 62},
 		sounds = def.sounds or default.node_sound_wood_defaults(),
 		drop = name .. "_bottom",
 		node_box = {
